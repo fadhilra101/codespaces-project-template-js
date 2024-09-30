b79c7129211e2beb3f008559975c6a8ff81816a7
Fadhil Raihan Akbar <101170882+fadhilra101@users.noreply.github.com>
2024-09-30 13:11:02 +0000

    Update package.json and App.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Fadhil RA",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "fadhil.ra101@gmail.com",
+  gitHub: "fadhilra101",
+  instagram: "@fadhilra_",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
